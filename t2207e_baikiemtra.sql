00106adc729c157f0d38b2c0233f97e27949fded
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2023-06-02 19:57:21 +0700

    Update t2207e_baikiemtra.sql

--- t2207e_baikiemtra.sql
+++ t2207e_baikiemtra.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 26, 2023 at 04:32 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.31
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
